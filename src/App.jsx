f09683abf0f3dd681a5d07c4e7e3dff580d086aa
15244862506 <2320951610@qq.com>
2019-11-28 20:24:13 +0800

    初始化提交

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import { Link } from 'react-router-dom';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>我是根组件</div>
+    )
+  }
+}
+export default App
